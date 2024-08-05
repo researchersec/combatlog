880e20038e2c471f7d511a0edbf10b87c36f5dc8
researchersec <109282836+researchersec@users.noreply.github.com>
2024-08-06 00:39:26 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-  server: {
-    port: 3000
-  },
+  root: path.resolve(__dirname, 'public'), // Set the root directory
   build: {
-    outDir: 'dist'
+    outDir: path.join(__dirname, 'dist'), // Output directory for build files
+    rollupOptions: {
+      input: path.resolve(__dirname, 'public/index.html') // Explicitly set the entry point
+    }
   }
 });
